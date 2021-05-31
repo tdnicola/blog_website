6e204cf6cd10b3240f0774ef1ef03927a7b0ede2
tdnicola <tdnicola@gmail.com>
2021-05-31 14:01:22 -0400

    basic website layout. Changed photos to projects. added discord project. Working on adding additional projects.

--- pages/_document.js
+++ pages/_document.js
@@ -8,10 +8,11 @@ class MyDocument extends Document {
 
   render() {
     const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
+      title: 'Tony Nicola',
+      description: 'Tony Nicola portfolio and blog site.',
       image:
-        'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+        'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png',
+      content: 'Tony Nicola, python, developer, javascript, SQL, Data Analyst, Web developer, Portfolio, Remote, Automation'
     }
 
     return (
@@ -23,11 +24,8 @@ class MyDocument extends Document {
           <meta property="og:description" content={meta.description} />
           <meta property="og:title" content={meta.title} />
           <meta property="og:image" content={meta.image} />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@yourname" />
-          <meta name="twitter:title" content={meta.title} />
-          <meta name="twitter:description" content={meta.description} />
-          <meta name="twitter:image" content={meta.image} />
+          <meta property="og:content" content={meta.content} />
+
         </Head>
         <body>
           <Main />
---
+++ pages/projects/index.md
@@ -0,0 +1,7 @@
+---
+type: posts
+title: Projects
+date: 2021-03-18
+---
+
+# Posts
